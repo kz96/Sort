2469e1d99d417b5207058ab875e8e7aa254b181c
kz96 <kornel.zielinski96@gmail.com>
2018-04-24 18:11:49 +0200

    finalizing the code

--- main.cpp
+++ main.cpp
@@ -6,7 +6,12 @@
 
 using namespace std;
 
-void merge(int* A, int low, int high, int mid) {
+/*
+ * Funkcja scalania dwoch rownych podtablic
+ */
+
+template <class T>
+void merge(T* A, int low, int high, int mid) {
 
         int i, j, k;
         i = low;
@@ -47,7 +52,12 @@ void merge(int* A, int low, int high, int mid) {
 
 }
 
-void mergeSort (int* A, int low, int high) {
+/*
+ * Rekurencyjny wariant algorytmu sortowania przez scalanie, tworzy dwie rowne podtablice, o liczbie elementow wiekszej
+ * niz 1, nastepnie wywoluje dla nich samego siebie, na koniec dokonuje scalenia
+ */
+template <class T>
+void mergeSort (T* A, int low, int high) {
 
     if (low < high){
         int mid;
@@ -60,7 +70,13 @@ void mergeSort (int* A, int low, int high) {
     }
 }
 
-int partition (int* A, int low, int high) {
+/*
+ * Funkcja partycjonowania tablicy wzgledem elementu nazywanego piwotem, mniejsze od niego trafiaja na lewo, wieksze
+ * na prawo
+ */
+
+template <class T>
+int partition (T* A, int low, int high) {
     int x = A[low];
     int i = low, j = high, temp;
 
@@ -85,18 +101,28 @@ int partition (int* A, int low, int high) {
     }
 }
 
-void quickSort (int* A, int low, int high) {
-    int border;
+/*
+ * Rekurencyjny wariant algorytmu sortowania szybkiego, po dokonaniu partycjnowania tablicy wzgledem piwota
+ * wywoluje samego siebie dla dwoch podtablic
+ */
+template <class T>
+void quickSort (T* A, int low, int high) {
+    int x;
     if (low < high) {
-        border = partition(A,low,high);
-            quickSort(A, low, border);
-            quickSort(A, border+1, high);
+        x = partition(A,low,high);
+            quickSort(A, low, x);
+            quickSort(A, x+1, high);
     }
 }
 
-void maxHeapify (int* A, int heapSize, int i){
-    int low = (i+1) * 2 - 1;
-    int high = (i+1) * 2;
+/*
+ * Funkcja budujaca kopiec z elementow nieposortowanej tablicy, nowo dodane elementy sa porownywane i umieszczane w
+ * odpowiednich miejscach, majac na uwadze ich rozmiar,
+ */
+template <class T>
+void maxHeapify (T* A, int heapSize, int i){
+    int low = (i + 1) * 2 - 1;
+    int high = (i + 1) * 2;
     int largest = 0;
 
     if (low < heapSize && A[low] > A[i]) {
@@ -119,14 +145,19 @@ void maxHeapify (int* A, int heapSize, int i){
     }
 }
 
-void heapSort (int* A, int arraySize) {
+/*
+ * Algorytm sortowania przez kopcowanie, elementy na jego dole tworza czesc posortowana, druga czesc zostaje naprawiona
+ * procedura jest powtarzana az do posortowania calosci
+ */
+template <class T>
+void heapSort (T* A, int arraySize) {
     int heapSize = arraySize;
 
     for(int j = (heapSize - 1) / 2; j >= 0; j--) {
         maxHeapify(A, heapSize, j);
     }
 
-    for (int i = arraySize -1; i > 0; i--) {
+    for (int i = arraySize - 1; i > 0; i--) {
         int temp = A[i];
         A[i] = A[0];
         A[0] = temp;
@@ -136,7 +167,12 @@ void heapSort (int* A, int arraySize) {
     }
 }
 
-void insertionSort (int* A, int arraySize) {
+/*
+ * Porownuje pierwszy element tablicy, znajdujacy sie w "czesci posortowanej", z dalszymi elemnetami i dokonuje
+ * ewentualnego "swapu"
+ */
+template <class T>
+void insertionSort (T* A, int arraySize) {
     int i,j,x;
     for (i = 1; i < arraySize; i++) {
         x = A[i];
@@ -147,7 +183,12 @@ void insertionSort (int* A, int arraySize) {
     }
 }
 
-void introSort (int* A, int arraySize) {
+/*
+ * Jeden z wariantow hybrydowego sortowania introspektywnego, w zaleznosci od stalej okreslajacej maksymalna
+ * dozwolona globokosc rekurencyjnych wywolan zostaje wywolany odpowiedni algorytm sortujacy
+ */
+template <class T>
+void introSort (T* A, int arraySize) {
     int partitionSize = partition(A, 0, arraySize-1);
 
     if (partitionSize < 16) {
@@ -161,50 +202,118 @@ void introSort (int* A, int arraySize) {
     }
 }
 
-void reverse (int* A, int arraySize) {
+/*
+ * Pomocnicza funkcja odwracajaca tablice "do gory nogami"
+ */
+template <class T>
+void reverse (T* A, int arraySize) {
     for (int i = 0; i < (arraySize/2); i++) {
-        swap(A[i],A[arraySize-1-i]);
+        T temp = A[i];
+        A[i] = A[arraySize-1-i];
+        A[arraySize-1-i] = temp;
     }
+}
+
+/*
+ * Pomocniczna funckja sluzaca do wyswietlania elementow tablicy
+ */
+template <class T>
+void print(T* A, int arraySize) {
+    for (int i = 0; i < arraySize; i++) {
+        cout << A[i] << endl;
+    }
+}
+
+/*
+ * Pomocnicza funkcja sluzaca do wyboru odpowiedniego sortowania w menu
+ */
+template <class T>
+void sorting(T* A, int arraySize) {
+    cout << "Wybierz sposob sortowania\n1. QUICK SORT\n2. MERGE SORT\n3. INTRO SORT" << endl;
+    char wybor;
+    cin >> wybor;
+    switch (wybor) {
+        case '1':
+            cout << "Przed sortowaniem" << endl;
+            print(A, arraySize);
+            //clock_t start = clock();
+            quickSort(A, 0, arraySize - 1);
+            //clock_t stop = clock();
+            //double czas = (double) (stop - start) / CLOCKS_PER_SEC;
+            cout << "Po sortowaniu" << endl;
+            print(A, arraySize);
+            break;
+        case '2':
+            cout << "Przed sortowaniem" << endl;
+            print(A, arraySize);
+            //start = clock();
+            mergeSort(A, 0, arraySize - 1);
+            //stop = clock();
+            //czas = (double) (stop - start) / CLOCKS_PER_SEC;
+            cout << "Po sortowaniu" << endl;
+            print(A, arraySize);
+            //cout << "CZAS" << czas << endl;
+            break;
+        case '3':
+            cout << "Przed sortowaniem" << endl;
+            print(A, arraySize);
+            //start = clock();
+            introSort(A, arraySize);
+            //stop = clock();
+            //czas = (double) (stop - start) / CLOCKS_PER_SEC;
+            cout << "Po sortowaniu" << endl;
+            print(A, arraySize);
+            //cout << "CZAS" << czas << endl;
+            break;
+        default:
+            cout << "Zła dana" << endl;
+
+    }
+}
+
+void menu() {
+    cout << "\n\nMENU" << endl;
+    cout << "1. SORTING" << endl;
+    cout << "2. REVERSE" << endl;
+    cout << "3. CLEAR" << endl;
+    cout << "4. END" << endl;
 }
 
 int main() {
     srand(time(NULL));
 
-    int arraySize;
-    cout << "Podaj dlugosc tablicy" << endl;
-    cin >> arraySize;
+    char c;
 
+    while(c != '4') {
+        menu();
+        cin >> c;
 
-    int *A;
-    A = new int [arraySize];
-    int low = 0;
-    int high = arraySize - 1;
+        switch (c) {
+            case '1':
+                int arraySize;
+                cout << "Podaj dlugosc tablicy" << endl;
+                cin >> arraySize;
+                int *A;
+                A = new int[arraySize];
 
+                for (int i = 0; i < arraySize; i++) {
+                    A[i] = rand() % 9999 + 1;
+                }
+                sorting(A,arraySize);
+                break;
+            case '2':
+                reverse(A,arraySize);
+                print(A,arraySize);
+                break;
+            case '3':
+                delete A;
+                cout << "Tablica usunieta !" << endl;
+                break;
+            default:
+                cout << "ZLA DANA" << endl;
+                break;
 
-    ofstream myfile;
-
-     for (int j=1; j < 101; j++) {
-
-
-        for (int i = 0; i < arraySize; i++) {
-            A[i] = rand() % 9999 + 1;
-            //cout << A[i] << endl;
         }
+    }
 
-        quickSort(A,low, ((0.997*arraySize)-1));
-
-        clock_t start = clock();
-        introSort(A, arraySize);
-        clock_t stop = clock();
-
-        double czas = (double) (stop - start) / CLOCKS_PER_SEC;
-
-
-
-        myfile.open("1000000.txt", ios::app);
-        myfile << j << ". " << czas << endl;
-        myfile.close();
-   }
-
-    delete A;
 }
